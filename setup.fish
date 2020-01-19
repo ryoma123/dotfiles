eb4790b3d7b92d17aa069b6f9361474a46e5843e
ryoma123 <ryoma.ono.2661@gmail.com>
2020-01-19 19:00:23 +0900

    Install dein.vim

--- setup.fish
+++ setup.fish
@@ -2,4 +2,11 @@
 if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 end
-fisher
+
+# Install dein.vim
+if not [ -f ~/.installer.sh ]
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > .installer.sh
+    sh ./installer.sh ~/.cache/dein
+end
+
+echo "Successfully installed!! Run 'fisher' if necessary."
